eedc3d3c0a171eb01eda09d4580690111980ab25
Tomek <143935926+Tome1000@users.noreply.github.com>
2024-04-28 18:57:12 +0200

    Added bootstrap and did little changes to code

---
+++ TODOLists/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace TODOLists.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<TODOLists.Data.TODOListsContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(TODOLists.Data.TODOListsContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
